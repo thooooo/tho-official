7b738a6750bce262da2f1aa7683feb7a075afbd2
Theo Midrouillet <theomidrouillet@gmail.com>
2022-10-25 08:03:01 +0200

    Added porjects caroussel

--- script.js
+++ script.js
@@ -513,3 +513,28 @@ $(doc).ready(function() {
     translate('fr', 'lng-tag');
   });
 });
+
+// Caroussel
+
+var $caroussel = $('#caroussel');
+var faces = $caroussel.find('figure');
+var panelSize = faces.outerWidth(true);
+var numberOfPanels = faces.length;
+var ty = 360 / numberOfPanels;
+var rad = Math.round( ( panelSize / 2) / Math.tan( Math.PI / numberOfPanels ) );
+var y = 0;
+
+faces.each(function(i,e){
+  $(e).css({'transform' : 'rotateY('+(i*ty)+'deg) translateZ('+rad+'px)'});
+});
+
+$caroussel.click(function(){
+	y += ty*-1;
+	$caroussel.css({
+		'transform': 'translateZ(-'+rad+'px) rotateY(' + y + 'deg)'
+	})
+});
+
+$caroussel.css({
+		'transform': 'translateZ(-'+panelSize+'px)'
+	})
